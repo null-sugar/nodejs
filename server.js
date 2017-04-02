cd5c8075ee917b404e5a494abfbc66741844650d
kukulu <kukukukukulu.t@gmail.com>
2017-04-02 16:12:22 +0900

    一行post

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 var http = require('http'),
-fs = require('fs'),
-ejs = require('ejs');
+	fs = require('fs'),
+	ejs = require('ejs'),
+	qs = require('querystring');
 var server = http.createServer();
 var settings = require('./settings');
-var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
-var n = 0;
-server.on('request', function (req, res) {
-		 	n++;
-
-	var data = ejs.render(template,{
-		title: "hello",
-		content: "<strong>world</strong>",
-		n: n
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
+var posts = [];
+function renderForm(posts, res){
+	var data = ejs.render(template, {
+		posts: posts
 	});
-	 res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.writeHead(200, {'Content-Type': 'text/html' });
+	res.write(data);
+	res.end();
+}
 
-  res.write(data);
-  res.end();
-console.log(n);
-	
+server.on('request', function (req, res) {
+	if( req.method == 'POST'){
+		req.data = "";
+		req.on("readable", function(){
+		req.data += req.read();
+		});
+		req.on("end", function(){
+			var query = qs.parse(req.data);
+			posts.push(query.name);
+			renderForm(posts,res);
+		});
+	} else{
+		renderForm(posts, res);
+	}
 });
 server.listen(settings.port, settings.host);
 console.log("server listening ...");
